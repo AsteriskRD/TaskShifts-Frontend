e8dc21cdca1df4a0b5f9ed4af048b03fbc402a48
Ojiteli Kosisochukwu <obuekwekosi@gmail.com>
2025-11-03 00:19:44 +0100

    created about page

---
+++ src/app/aboutus/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import about from "../images/creators.png"
+
+const AboutUs = () => {
+  return (
+    <div className='pt-30 max-w-[1440px] mx-auto '>
+        <h1>About Us</h1>
+    </div>
+  )
+}
+
+export default AboutUs
