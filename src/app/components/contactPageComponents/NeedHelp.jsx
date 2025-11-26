a599ea3fa233a43cad6cb2145a8ae43c3fefb3f1
Ojiteli Kosisochukwu <obuekwekosi@gmail.com>
2025-11-27 00:11:27 +0100

    updated contact page

---
+++ src/app/components/contactPageComponents/NeedHelp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NeedHelp = () => {
+  return (
+    <div className='px-6 sm:px-12 py-12  md:px-10'>
+         <div className='bg-[#002A5E] text-center  py-10'>
+        <h1 className='font-bold text-center text-2xl text-white'>Need Immediate Help?</h1>
+        <p className='text-[#E6E6EE] py-3'>Our Live Chat support is available during business hours for quick assistance with urgent matters</p>
+        <div className='py-8'>
+            <button className='bg-[#FF6815] px-3 py-2 rounded-md text-white'>Get Started to Start Live Chat</button>
+        </div>
+      
+    </div>
+    </div>
+   
+  )
+}
+
+export default NeedHelp
